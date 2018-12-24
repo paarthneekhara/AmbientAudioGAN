4f8876286d9d175c5f6b8970b4df87f69ac3a622
Paarth Neekhara <paarth.n@gmail.com>
2018-12-24 17:46:25 +0530

    nothing happened

--- deepyeti_training.sh
+++ deepyeti_training.sh
@@ -1,20 +1,40 @@
 export CUDA_VISIBLE_DEVICES="0"
-TRAIN_DIR=/data2/paarth/TrainDir/Ambient/tatum_dp_skip
+TRAIN_DIR=/data2/paarth/TrainDir/Ambient/tatum_dp_standardAE
 rm -rf ${TRAIN_DIR}
 python3 train_evaluate.py train \
 	${TRAIN_DIR} \
 	--data_dir /data2/paarth/ambient/clipped_dp_512_04/tatum/train \
 	--data_fastwav \
-	--model_overrides "objective=l1,batchnorm=False,train_batch_size=64,alpha=100.0,enc_length=64" \
+	--model_overrides "objective=l1,batchnorm=False,train_batch_size=64,alpha=100.0,enc_length=64,use_skip=False" \
 	--train_summary_every_nsecs 60 
 
-export CUDA_VISIBLE_DEVICES="0"
-TRAIN_DIR=/datasets/home/10/610/pneekhar/TRAIN/Ambient/sc09_dp_skip
+export CUDA_VISIBLE_DEVICES="-1"
+TRAIN_DIR=/data2/paarth/TrainDir/Ambient/tatum_dp_standardAE
 python3 train_evaluate.py eval \
 	${TRAIN_DIR} \
-	--data_dir /datasets/home/10/610/pneekhar/DATA/clipped_dp_256_04/sc09/valid \
+	--data_dir /data2/paarth/ambient/clipped_dp_512_04/tatum/valid \
 	--data_fastwav \
-	--model_overrides "objective=l1,batchnorm=False,train_batch_size=2,alpha=100.0,enc_length=64"
+	--model_overrides "objective=l1,batchnorm=False,train_batch_size=2,alpha=100.0,enc_length=64,use_skip=False"
+
+
+
+export CUDA_VISIBLE_DEVICES="1"
+TRAIN_DIR=/data2/paarth/TrainDir/Ambient/tatum_dp_skip_stride2
+rm -rf ${TRAIN_DIR}
+python3 train_evaluate.py train \
+	${TRAIN_DIR} \
+	--data_dir /data2/paarth/ambient/clipped_dp_512_04/tatum/train \
+	--data_fastwav \
+	--model_overrides "objective=l1,batchnorm=False,train_batch_size=64,alpha=100.0,enc_length=16,use_skip=False,stride=2,skip_limit=4" \
+	--train_summary_every_nsecs 60 
+
+export CUDA_VISIBLE_DEVICES="-1"
+TRAIN_DIR=/data2/paarth/TrainDir/Ambient/tatum_dp_skip_stride2
+python3 train_evaluate.py eval \
+	${TRAIN_DIR} \
+	--data_dir /data2/paarth/ambient/clipped_dp_512_04/tatum/valid \
+	--data_fastwav \
+	--model_overrides "objective=l1,batchnorm=False,train_batch_size=2,alpha=100.0,enc_length=64,use_skip=False,stride=2,skip_limit=4"
 
 
 export CUDA_VISIBLE_DEVICES="1"
